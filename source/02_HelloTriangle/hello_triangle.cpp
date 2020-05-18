8e5b553004b3357958e0e4df2f969ee7cbb01f07
Paul Tunison <paul.tunison@kitware.com>
2020-05-18 03:21:17 -0400

    Added glfw window stuff

--- source/02_HelloTriangle/hello_triangle.cpp
+++ source/02_HelloTriangle/hello_triangle.cpp
@@ -1,48 +1,92 @@
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 #include <stdexcept>
 
+#include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
 
 
 class HelloTriangleApp
 {
 public:
-    ~HelloTriangleApp() = default;
+  char const* WIN_TITLE = "HelloTriangle";
 
-    void run()
-    {
-        initVulkan();
-        mainLoop();
-        cleanUp();
-    }
+  explicit HelloTriangleApp(uint32_t window_height = 600,
+                            uint32_t window_width = 800)
+      : win_height(window_height), win_width(window_width), window(nullptr)
+  {}
+
+  ~HelloTriangleApp() = default;
+
+  void run()
+  {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    // Call in destructor instead?
+    cleanUp();
+  }
 
 private:
-    void initVulkan()
-    {
-    }
+  uint32_t win_height, win_width;
+  GLFWwindow *window;
 
-    void mainLoop()
+  void initWindow()
+  {
+    int glfw_init_ret = glfwInit();
+    if (glfw_init_ret != GLFW_TRUE)
     {
+      std::ostringstream ss;
+      ss << "glfwInit() returned failure code " << glfw_init_ret;
+      throw std::runtime_error(ss.str());
     }
+    // Prevent creation of OpenGL context.
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    // Disable window resizing
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    void cleanUp()
+    // To make full-screen, `monitor` will need to be specified and the
+    // specified size will be the resolution, otherwise will be in windowed
+    // mode.
+    this->window = glfwCreateWindow(
+        win_width, win_height, HelloTriangleApp::WIN_TITLE,
+        nullptr, nullptr
+    );
+  }
+
+  void initVulkan()
+  {}
+
+  void mainLoop()
+  {
+    while (!glfwWindowShouldClose(window))
     {
+      glfwPollEvents();
     }
+  }
+
+  void cleanUp()
+  {
+    glfwDestroyWindow(this->window);
+    this->window = nullptr;
+    glfwTerminate();
+  }
 };
 
-
-int main()
+int
+main()
 {
-    HelloTriangleApp app;
-    try
-    {
-        app.run();
-    }
-    catch (const std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+  HelloTriangleApp app;
+  try
+  {
+    app.run();
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << "Caught exception (type: " << typeid(e).name() << "): "
+              << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
