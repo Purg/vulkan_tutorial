86cd657fe28914bca53c8f9e2851e33929904273
Paul Tunison <paul.tunison@kitware.com>
2020-06-05 20:25:43 -0400

    Added some spiffy filenames, numbers and funcs to logging

--- myengine/logging.hpp
+++ myengine/logging.hpp
@@ -1,11 +1,21 @@
 /**
  * Basic logging macros.
+ *
+ * I would do `constexpr` functions but I don't know how to represent the `<<`-able type...
  */
 
 #ifndef LOGGING_HPP
 #define LOGGING_HPP
 
+#include <cstring>
 #include <iostream>
+
+
+// Not sure if this is going to cause a performance issue. If so maybe give up width-saving
+// convenience for the simple use of __FILE__?
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define _LOG_LOC \
+  __FILENAME__ << "(" << __LINE__ << ")[" << __func__ << "]"
 
 
 #ifdef NDEBUG
@@ -14,20 +24,20 @@
 #define LOG_DEBUG( msg ) \
   do \
   { \
-    std::cerr << "[BASIC][DEBUG] " << msg << std::endl; \
+    std::cerr << "[DEBUG] " << _LOG_LOC << " " << msg << std::endl; \
   } while(false)
 #endif
 
 #define LOG_INFO( msg ) \
   do \
   { \
-    std::cerr << "[BASIC][ INFO] " << msg << std::endl; \
+    std::cerr << "[ INFO] " << _LOG_LOC << " " << msg << std::endl; \
   } while(false)
 
 #define LOG_ERROR( msg ) \
   do \
   { \
-    std::cerr << "[BASIC][ERROR] " << msg << std::endl; \
+    std::cerr << "[ERROR] " << _LOG_LOC << " " << msg << std::endl; \
   } while(false)
 
 #endif //LOGGING_HPP
